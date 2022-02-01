ca46f45e68a130ab5034915c3e1236f70430c13e
MyShlongIsLongAT <florian_breuker@outlook.com>
2022-02-01 20:22:06 +0100

    added working delete function

--- installation_basics/todolist/src/components/todo.js
+++ installation_basics/todolist/src/components/todo.js
@@ -4,27 +4,21 @@ import { GrClose } from "react-icons/gr";
 import styles from "./todo.module.css";
 
 class ToDo extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      id: this.props.id,
-    };
-  }
-
   //edit for remove of task
   removeTask = () => {
-    this.props.isDone(this.state.id);
+    console.log("Delete id: " + this.props.id);
+    this.props.isDone(this.props.id);
   };
 
   render() {
     return (
       <div className={styles.container}>
-        <div>{this.props.title}</div>
+        <div>
+          {this.props.title}:{this.props.id}
+        </div>
         <div className={styles.icons} onClick={this.removeTask}>
           <GrClose />
         </div>
-        {console.log(this.state.id)}
       </div>
     );
   }
